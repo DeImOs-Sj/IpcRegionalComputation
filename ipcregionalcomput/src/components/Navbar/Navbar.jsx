268f18302d903c4c22aa2f212cd90991a2f48b8d
Your Name <shlokjagtap123@gmail.com>
2024-03-26 21:39:18 +0530

    rainbow kit addition

--- ipcregionalcomput/src/components/Navbar/Navbar.jsx
+++ ipcregionalcomput/src/components/Navbar/Navbar.jsx
@@ -45,6 +45,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 
 const Navbar = () => {
   return (
@@ -141,16 +143,8 @@ const Navbar = () => {
           </SheetContent>
         </Sheet>
         <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-          <form className="ml-auto flex-1 sm:flex-initial">
-            <div className="relative">
-              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="search"
-                placeholder="Search products..."
-                className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-              />
-            </div>
-          </form>
+          <ConnectButton />
+        
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="secondary" size="icon" className="rounded-full">
--- ipcregionalcomput/src/main.jsx
+++ ipcregionalcomput/src/main.jsx
@@ -3,14 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { WagmiProvider } from 'wagmi' 
+import { config } from './Rainbowkit.jsx' 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query' 
 
+
+const queryClient = new QueryClient() 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
+    <WagmiProvider config={config}> 
+            <QueryClientProvider client={queryClient}> 
+
+
       <BrowserRouter>
 
       <App />
-        </BrowserRouter>,
+        </BrowserRouter>
+              </QueryClientProvider> 
+
+          </WagmiProvider> 
+
 
   </React.StrictMode>,
 )
